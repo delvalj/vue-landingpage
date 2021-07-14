caa4ab992c9ea1db578cc64d4da1299d02dfef9b
Joaquin del Val <delvaljoaquin@gmail.com>
2021-07-14 12:16:53 -0300

    sigo avanzando componente reutilizable

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -1,5 +1,25 @@
 <template>
-  {{ content }}
+  <div class="Presentation flex flex-col md:flex-row" :class="{ 'md:flex-row-reverse': content.left}">
+<!--    la clase que le estoy bindeando se lee: cuando el content este del lado izquierdo aplicar la clase
+FLEX ROW REVERSE que esuna funcionalidad de flex -->
+    <div v-for="col, colIndex in content.columns" :key="colIndex" class="flex-1">
+      <div v-if="col.imgSrc">
+        <img :src="col.imgSrc" alt="image">
+      </div>
+        <div v-else class="p-8 md:p-24" :class="`bg-${col.backgroundColor} text-${ content.left ? 'black' : 'white'}`">
+          <div class="mb-16">
+          <h2 class="text-4xl font-bold uppercase mb-4">{{ col.title }}</h2>
+          <div class=" h-1 w-16" :class="`bg-${ content.left ? 'black' : 'white'}`">
+
+          </div>
+        </div>
+
+        <p class="font-bold uppercase mb-4">{{ col.subtitle }}</p>
+        <p class="font-light">{{ col.description }}</p>
+      </div>
+
+    </div>
+  </div>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,7 @@
   <div>
       <Nav></Nav>
     <MainHeader> </MainHeader>
-    <p>{{ hello }}</p>
     <Presentation  v-for="obj, objIndex in presentations" :key="objIndex" :content="obj"></Presentation>
-
 
 <!--    <Stats></Stats>-->
 <!--    <Tabs></Tabs>-->
@@ -16,7 +14,8 @@
 <script >
 import { MainHeader, Nav, Presentation, Stats, Tabs, Footer } from '../components';
 import { reactive, toRefs } from 'vue' ;
-import fotopequeña1 from '../assets/pequeña1.jpg';
+import medio2 from '../assets/medio2.jpg';
+import medio1 from '../assets/medio1.jpg';
 
 export default {
   components: {
@@ -27,22 +26,41 @@ export default {
   setup() {
     let data = reactive({
       presentations: [{
-          left: true,
+          left: false,
         columns: [{
+            backgroundColor: "customCream",
           title: 'Visita Argentina',
           subtitle: 'Un pais con buena gente',
           description: 'La Argentina tiene muchas particularidades que han recorrido el mundo en boca de sus visitantes.' +
               ' La personalidad desenvuelta de su gente, amigable, divertida y efusiva; la gastronomía típica que va desde' +
               ' el asado hasta el mate, pasando por los platos regionales y bocaditos con dulce de leche; las danzas tradicionales' +
               ' como el tango, baile de parejas a un ritmo de 2 por 4 que nace en Buenos Aires en el siglo XIX y hoy es difundido' +
-              ' internacionalmente,' +' ó el folklore, con las diferentes formas coreográficas que adopta en cada región;' +
-              'Imposible olvidar, la pasión por el fútbol'
+              ' internacionalmente'
+        },{
+            imgSrc: medio2
+          }]
+        }, {
+          left: true,
+          columns: [{
+            backgroundColor: "white",
+            button: {
+              backgroundColor: "customCream",
+              text: 'Elige tu Destino'
+            },
+
+            title: 'Visita Argentina',
+            subtitle: 'Un pais con buena gente',
+            description: 'La Argentina tiene muchas particularidades que han recorrido el mundo en boca de sus visitantes.' +
+                ' La personalidad desenvuelta de su gente, amigable, divertida y efusiva; la gastronomía típica que va desde' +
+                ' el asado hasta el mate, pasando por los platos regionales y bocaditos con dulce de leche; las danzas tradicionales' +
+                ' como el tango, baile de parejas a un ritmo de 2 por 4 que nace en Buenos Aires en el siglo XIX y hoy es difundido' +
+                ' internacionalmente,' +' ó el folklore, con las diferentes formas coreográficas que adopta en cada región;' +
+                'Imposible olvidar, la pasión por el fútbol'
+          },{
+            imgSrc: medio1
+          }]
         },
-          {
-            imgSrc: fotopequeña1
-          },
-        ],
-        }]
+      ]
       })
 
     return{
