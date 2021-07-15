81f22e9442cbc89071ac0f949d3eb7186034f837
Joaquin del Val <delvaljoaquin@gmail.com>
2021-07-15 18:21:33 -0300

    cambio imagenes

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="Stats">
-     <p>This is my Stats Component</p>
-    <stats-card></stats-card>
+  <div class="Stats bg-cover bg-bottom bg-center bg-no-repeat" :style="`background-image:url(${grande3horizontal}); height:450px`">
+    <div class="w-full h-full bg-customCream opacity-50 absolute"></div>
+    <div class="Stats-Components z-50 relative font-bold">
+        <p class="text-white">some text </p>
+    </div>
   </div>
 </template>
 
+
 <script setup>
-import StatsCard from './elements/stats/StatsCard.vue'
+import StatsCard from './elements/stats/StatsCard.vue';
+import grande3horizontal from '../assets/grande2.jpg';
 </script>
 
