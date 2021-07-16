d33639b5f27ea28dc39583d6efa7c597519567fd
Joaquin del Val <delvaljoaquin@gmail.com>
2021-07-16 12:35:51 -0300

    quito clase que no servia

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -3,15 +3,15 @@
   <div class="Presentation lg:flex flex-col lg:flex-row " :class="{ 'lg:flex-row-reverse': content.left}">
 <!--    la clase que le estoy bindeando se lee: cuando el content este del lado izquierdo aplicar la clase
 FLEX ROW REVERSE que esuna funcionalidad de flex -->
-    <div v-for="col, colIndex in content.columns" :key="colIndex" class="flex-1">
+    <div v-for="col, colIndex in content.columns" :key="colIndex" class="flex-1 ">
       <div v-if="col.imgSrc">
-        <img :src="col.imgSrc" alt="image">
+        <img :src="col.imgSrc" alt="image" >
       </div>
         <div v-else class="p-24" :class="`bg-${col.backgroundColor} text-${ content.left ? 'black' : 'white'}`">
-          <div class="mb-16">
-          <h2 class="text-4xl font-bold uppercase mb-4">{{ col.title }}</h2>
-          <div class="h-1 w-16" :class="`bg-${ content.left ? 'black' : 'white'}`">
-
+          <div class="mb-8">
+          <h2 class="text-4xl font-bold uppercase mb-20">{{ col.title }}</h2>
+<!--          subrayado de visita argentina  es el h-1 w-16 -->
+            <div class="h-1 w-16">
           </div>
         </div>
 
